cfe1b0da5b28f1c7b5c8fd5609493256f372c656
github username <joshmrallen@gmail.com>
2021-01-10 16:26:52 -0500

    added to routes and users_controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,7 +8,17 @@ class UsersController < ApplicationController
         @user = User.find(params[:id])
     end
 
-    # def edit
-        
-    # end
+    def edit
+        @user = current_user
+    end
+
+    def delete_image
+        image = ActiveStorage::Attachment.find(params[:image_id])
+        if current_user = image.record || current_user.admin?
+            image.purge
+            #message or jwt that front end will use to redirect after deletion
+        else
+            #error message and value/jwt that frontend will use to redirect with an error message
+        end
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :users, only: [:index, :show, :edit, :update] do
-    # member do
-    #   delete 'delete_image/:image_id', action: 'delete_image', as: 'delete_image'
-    # end
+    member do
+      delete 'delete_image/:image_id', action: 'delete_image', as: 'delete_image'
+    end
   end
 end
